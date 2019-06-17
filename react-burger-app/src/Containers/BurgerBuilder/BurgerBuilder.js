d677f65a16a61f9c2bb9776e9d92864ac6f3a9c9
Hosseinabadi <shahram.hosseinabadi@pearson.com>
2019-06-17 14:25:08 +1000

    saving all

--- react-burger-app/src/Containers/BurgerBuilder/BurgerBuilder.js
+++ react-burger-app/src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,14 @@ import Auxiliary from '../../hoc/Auxiliary';
 import Burger from '../../Components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2,
+        }
+    }
     render() {
         return (
             <Auxiliary>
