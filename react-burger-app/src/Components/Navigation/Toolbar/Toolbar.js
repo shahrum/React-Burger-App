6de514a8f0d9598116735f481f2da81cbd52908a
Hosseinabadi <shahram.hosseinabadi@pearson.com>
2019-06-28 13:11:14 +1000

    adding toolbar

---
+++ react-burger-app/src/Components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import cls from './Toolbar.css';
+
+const toolbar = (props) => (
+    <header className={cls.Toolbar}>
+        <div>MENU</div>
+        <div>LOGO</div>
+        <nav>...</nav>
+    </header>
+);
+
+export default toolbar;
