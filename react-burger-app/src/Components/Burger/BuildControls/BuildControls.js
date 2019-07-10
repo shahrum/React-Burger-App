e08214dc7bc8162f7b6923d763ece22a16c93279
Hosseinabadi <shahram.hosseinabadi@pearson.com>
2019-07-10 14:18:37 +1000

    adding improvement and responsiveness

--- react-burger-app/src/Components/Burger/BuildControls/BuildControls.js
+++ react-burger-app/src/Components/Burger/BuildControls/BuildControls.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import cls from './BuildControls.css';
+
+import classes from './BuildControls.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    {label: 'Salad', type: 'salad'},
-    {label: 'Bacon', type: 'bacon'},
-    {label: 'Cheese', type: 'cheese'},
-    {label: 'Meat', type: 'meat'}
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
-    <div className={cls.BuildControls}>
+    <div className={classes.BuildControls}>
         <p>Current Price: <strong>{props.price.toFixed(2)}</strong></p>
         {controls.map(ctrl => (
-            <BuildControl
-                key={ctrl.type}
+            <BuildControl 
+                key={ctrl.label} 
                 label={ctrl.label}
                 added={() => props.ingredientAdded(ctrl.type)}
-                removed={() => props.ingredinetRemovd(ctrl.type)}
+                removed={() => props.ingredientRemoved(ctrl.type)}
                 disabled={props.disabled[ctrl.type]} />
         ))}
-        <button className={cls.OrderButton} disabled={!props.purchasable} onClick={props.ordered}>ORDER NOW</button>
+        <button 
+            className={classes.OrderButton}
+            disabled={!props.purchasable}
+            onClick={props.ordered}>ORDER NOW</button>
     </div>
 );
 
