9ef17ae6168079e986e429b5df36723033c2bd44
Hosseinabadi <shahram.hosseinabadi@pearson.com>
2019-07-11 11:51:49 +1000

    changes

--- react-burger-app/src/App.js
+++ react-burger-app/src/App.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
-import Layout from './Components/Layout/Layout';
-import BurgerBuilder from './Containers/BurgerBuilder/BurgerBuilder';
+import React, { Component } from 'react';
+
+import Layout from './hoc/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
-  render() {
+  render () {
     return (
-    <div>
-      <Layout>
-        <BurgerBuilder />
-      </Layout>
-    </div>
-  );
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+      </div>
+    );
   }
 }
 
