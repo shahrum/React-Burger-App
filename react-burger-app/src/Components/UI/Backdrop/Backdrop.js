7ef526decdaa5a62c61098ebf9367493896b7b7c
Hosseinabadi <shahram.hosseinabadi@pearson.com>
2019-08-09 15:30:40 +1000

    all files

--- react-burger-app/src/Components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import cls from './Backdrop.css';
-
-const backdrop = (props) => (
-    props.show ? <div className={cls.Backdrop} onClick={props.clicked}></div> : null
-);
-
-export default backdrop;
